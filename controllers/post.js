f7e5b6c4159e96b19a168faa55865a71bdba2ce8
MuizU <muizuvais99@gmail.com>
2022-04-16 18:32:14 +0530

    Added proper get and post requests with mongodb

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,16 @@
-exports.getPosts = (req, res) => {
-  res.json({
-    posts: [{ title: "First Post" }, { title: "Second Post" }],
-  });
+const Post = require("../models/post");
+
+exports.getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find().select("_id title body");
+    res.json({ posts });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+exports.createPost = async (req, res) => {
+  const post = new Post(req.body);
+  const result = await post.save();
+  res.status(200).json({ post: result });
 };
