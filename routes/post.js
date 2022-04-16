a16cfac59bfbac76f4199c444ed18f9e7c53594e
MuizU <muizuvais99@gmail.com>
2022-04-16 18:32:35 +0530

    Added the post route

--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
-const {getPosts} = require("../controllers/post.js");
+const { getPosts, createPost } = require("../controllers/post.js");
+const { createPostValidator } = require('../validator');
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get("/", getPosts);
+router.post("/posts", createPostValidator, createPost);
 
-module.exports = router
+module.exports = router;
